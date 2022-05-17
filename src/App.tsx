6b1bdf8a8398dafc774365e417c5050a3f87b92b
Olli Tanhuanpää <ottanh@utu.fi>
2022-05-17 21:24:56 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,20 @@ import NavigationBar from './NavigationBar.tsx';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
 import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 const App = () => {
+  const loggedIn = true;
   return (
     <Container className="App pt-5 border border-top-0 h-100">
       <Row className="h-100">
         <NavigationBar />
-        <UserProfilePage user={user} />
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/olli1111" />} />
+          <Route path="/olli1111" element={<UserProfilePage user={user} />} />
+          <Route path="/explore" element={<Col className="ExploreView col-md-5 h-100 d-flex flex-column">TODO</Col>} />
+          <Route path="/settings" element={<Col className="SettingsView col-md-5 h-100 d-flex flex-column">TODO</Col>} />
+        </Routes>
         <Col className="RightPadding col-sx-1" />
       </Row>
     </Container>
