bcf9abe4a857f11b2825ce16bb4bec0cc7d2bedc
Olli Tanhuanpää <ottanh@utu.fi>
2022-05-20 06:38:05 +0300

    remove /home route

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,14 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 const App = () => {
 
-  const loggedInUser = 'olli111';
+  const loggedInUser = null;
 
   return (
     <Container className="App pt-5 border border-top-0 min-vh-100">
       <Row className="min-vh-100">
         <NavigationBar />
         <Routes>
-          <Route path="/" element={loggedInUser ? <Navigate replace to="/olli111" />: <Navigate replace to="/home" />} />
-          <Route path="/home" element={<Col className="Home col-md-5 h-100 d-flex flex-column">TODO</Col>} />
+          <Route path="/" element={loggedInUser ? <Navigate replace to="/olli111" />: <Col className="Home col-md-5 h-100 d-flex flex-column">TODO</Col>} />
           <Route path="/:userName" element={<UserProfilePage />} />
           <Route path="/explore" element={<Col className="ExploreView col-md-5 h-100 d-flex flex-column">TODO</Col>} />
           <Route path="/settings" element={<Col className="SettingsView col-md-5 h-100 d-flex flex-column">TODO</Col>} />
