f44c206532ed6a60ae2ec15a09e2cc6ac557a1cd
Olli Tanhuanpää <ottanh@utu.fi>
2022-05-20 05:48:01 +0300

    add data fetch from backend

--- src/UserProfilePage/index.tsx
+++ src/UserProfilePage/index.tsx
@@ -1,31 +1,52 @@
-import { useEffect } from 'react';
 import Col from 'react-bootstrap/esm/Col';
 import { useParams } from 'react-router-dom';
 import PostList from '../PostList';
-import { useStateValue, setUser } from '../state';
 import UserDetails from './UserDetails';
-import Users from '../data/users';
+import { User } from '../types';
+
+import { gql, useQuery } from '@apollo/client';
+
+export const FIND_USER = gql`
+  query findUser($username: String!) {
+    findUser (username: $username) { 
+      username
+      name
+      joined
+      description
+      posts {
+        id
+        content
+        date
+        likes
+        user
+      }
+  }
+}`;
+
+interface UserQueryResult {
+   findUser: User 
+}
 
 const UserProfilePage = () => {
-
-  const [{ user }, dispatch] = useStateValue();
   const { userName } = useParams<{ userName: string }>();
+  const { loading, data } = useQuery<UserQueryResult>(FIND_USER, {
+    variables: { username: userName }
+  });
 
-  useEffect(() => {
-    const user = Users.find(user => user.username === userName);
-    if(user) {
-      dispatch(setUser(user));
-    }
-  }, [userName]);
 
-  if(!user){
+  if(loading) {
+    return <Col className="NoUserFound col-md-5 h-100 d-flex flex-column">Loading...</Col>;
+  }
+
+  if(!data){
     return <Col className="NoUserFound col-md-5 h-100 d-flex flex-column">No user found</Col>;
   }
 
+
   return (
     <Col className="UserProfilePage col-md-5 d-flex flex-column">
-      <UserDetails user={user} />
-      <PostList posts={user.posts} />
+      <UserDetails user={data.findUser} />
+      <PostList posts={data.findUser.posts} />
     </Col>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { reducer, StateProvider } from './state';
 
+import { 
+  ApolloClient, 
+  ApolloProvider, 
+  HttpLink, 
+  InMemoryCache
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  })
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Router>
-      <StateProvider reducer={reducer}>
-        <App />
-      </StateProvider>
+      <ApolloProvider client={client} >
+        <StateProvider reducer={reducer}>
+          <App />
+        </StateProvider>
+      </ApolloProvider>
     </Router>
   </React.StrictMode>
 );
