9a358cbc8fdffae9f4b51a801fd7a9b06b919b28
Olli Tanhuanpää <ottanh@utu.fi>
2022-05-24 06:38:28 +0300

    add StartPage and add routes /login /register

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NavigationBar from './NavigationBar.tsx';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import StartPage from './StartPage';
 
 const App = () => {
 
@@ -14,10 +15,12 @@ const App = () => {
       <Row className="min-vh-100">
         <NavigationBar />
         <Routes>
-          <Route path="/" element={loggedInUser ? <Navigate replace to="/olli111" />: <Col className="Home col-md-5 h-100 d-flex flex-column">TODO</Col>} />
+          <Route path="*" element={loggedInUser ? <Navigate replace to="/olli111" />: <StartPage />} />
+          <Route path="/login" element={<Col className="ExploreView col-md-5 h-100 d-flex flex-column">Login</Col>} />
+          <Route path="/register" element={<Col className="ExploreView col-md-5 h-100 d-flex flex-column">Register</Col>} />
           <Route path="/:userName" element={<UserProfilePage />} />
-          <Route path="/explore" element={<Col className="ExploreView col-md-5 h-100 d-flex flex-column">TODO</Col>} />
-          <Route path="/settings" element={<Col className="SettingsView col-md-5 h-100 d-flex flex-column">TODO</Col>} />
+          <Route path="/explore" element={<Col className="ExploreView col-md-5 h-100 d-flex flex-column">Explore</Col>} />
+          <Route path="/settings" element={<Col className="SettingsView col-md-5 h-100 d-flex flex-column">Settings</Col>} />
         </Routes>
         <Col className="RightPadding col-sx-1 ps-5" />
       </Row>
---
+++ src/StartPage/index.tsx
@@ -0,0 +1,12 @@
+import Col from 'react-bootstrap/esm/Col';
+import LogRegButtons from './LogRegButtons';
+
+const LoginPage = () => {
+  return (
+    <Col className="Home col-md-5 h-100 d-flex flex-column">
+      <LogRegButtons />
+    </Col>
+  );
+};
+
+export default LoginPage;
