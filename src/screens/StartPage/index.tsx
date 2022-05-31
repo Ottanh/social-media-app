028e84d9f94e6f8c0a0a2c9b040124f3bcf4e4df
Olli Tanhuanpää <ottanh@utu.fi>
2022-05-31 21:34:54 +0300

    add new StartPage

--- src/screens/StartPage/index.tsx
+++ src/screens/StartPage/index.tsx
@@ -1,10 +1,20 @@
 import LogRegButtons from '../../components/User/LogRegButtons/LogRegButtons';
 import './index.css';
+import manuli from '../../images/manuli.jpg';
+
 
 const StartPage = () => {
   return (
     <div className="StartPage">
-      <LogRegButtons />
+      <div className="ImageContainer">
+        <img src={manuli} alt="manuli"/>
+      </div>
+      <div className="TitleContainer">
+        <h1 className="Title">SomeThing</h1>
+        <LogRegButtons />
+        <div className="TitlePadding"/>
+      </div>
+      <div className="Padding" />
     </div>
   );
 };
