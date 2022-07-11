11e20a6b9e2385300f98c823465402b5b165d8da
Olli Tanhuanpää <ottanh@utu.fi>
2022-07-11 20:12:15 +0300

    fix scripts and config

--- src/index.tsx
+++ src/index.tsx
@@ -23,8 +23,9 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+console.log(process.env.NODE_ENV);
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000',
+  uri: process.env.NODE_ENV === 'production' ? 'http://ec2-16-170-237-56.eu-north-1.compute.amazonaws.com:4000' : 'http://localhost:4000',
 });
 
 
