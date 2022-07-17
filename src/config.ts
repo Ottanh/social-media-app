f643f6388a14a91b9d4fc5ab1be3ecd7dc202f45
Olli Tanhuanpää <ottanh@utu.fi>
2022-07-17 22:45:28 +0300

    fix config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URI = process.env.NODE_ENV === 'production' 
-  ? 'http://ec2-16-170-237-56.eu-north-1.compute.amazonaws.com:4000' 
+  ? 'http://ec2-16-170-237-56.eu-north-1.compute.amazonaws.com:80' 
   : 'http://localhost:4000';
