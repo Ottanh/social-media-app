65d7f01d1f3adb416a622115bda2397ca0c7b532
Olli Tanhuanpää <ottanh@utu.fi>
2022-07-19 21:19:01 +0300

    update backend ip

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URI = process.env.NODE_ENV === 'production' 
-  ? 'http://ec2-16-170-237-56.eu-north-1.compute.amazonaws.com:80' 
+  ? '13.53.219.140' 
   : 'http://localhost:4000';
